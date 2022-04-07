e4acbb319e63e7664c6fdfb94535a8b5d130b83a
caroline <carzhang96@gmail.com>
2022-04-07 12:37:12 -0400

    print error messages to the screen

--- signup.php
+++ signup.php
@@ -15,6 +15,28 @@
           <button type="submit" name="submit">Sign Up</button>
         </form>
       </div>
+      <?php
+      if (isset($_GET["error"])){
+        if ($_GET["error"] == "emptyinput") {
+          echo "<p>Fill in all fields</p>";
+        }
+        else if ($_GET["error"] == "invalidemail") {
+          echo "<p>Enter a proper email</p>";
+        }
+        else if ($_GET["error"] == "mismatchingpasswords") {
+          echo "<p>Passwords don't match</p>";
+        }
+        else if ($_GET["error"] == "stmtFailed") {
+          echo "<p>Something went wrong. Try again.</p>";
+        }
+        else if ($_GET["error"] == "emailalreadyexists") {
+          echo "<p>An account already exists with this email</p>";
+        }
+        else if ($_GET["error"] == "none") {
+          echo "<p>You have signed up</p>";
+        }
+      }
+    ?>
     </div>
 
 <?php 
